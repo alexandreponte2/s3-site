dad4e76afd3059556a11cd4915de1894de515c05
alexandreponte1 <alexandre.s.ponte@accenture.com.br>
2024-09-19 13:32:08 -0300

    update

---
+++ terraform/s3.tf
@@ -0,0 +1,61 @@
+# Provider AWS
+provider "aws" {
+region = var.region
+profile = var.aws_profile
+
+}
+
+# S3 bucket for hosting a static website
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = var.common_tags
+}
+
+# Bucket policy to allow public read access
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+
+
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+
+resource "aws_s3_bucket_cors_configuration" "example" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+
+# Output the website URL
+output "website_url" {
+  value = aws_s3_bucket.website_bucket.website_endpoint
+}
